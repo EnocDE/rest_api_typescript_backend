31fec649fd044057fe9ffae1b11648af20c7181e
EnocDE <enocescazan@hotmail.com>
2024-04-03 12:25:24 -0600

    Creación y configuración de los archivos iniciales del proyecto

---
+++ dist/server.js
@@ -0,0 +1,10 @@
+"use strict";
+// Configuraciones del servidor
+// Arquitectura handdler
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sumar = void 0;
+function sumar() {
+    console.log(2 + 2);
+}
+exports.sumar = sumar;
+//# sourceMappingURL=server.js.map
