bb014a6a85147d3c18f2bc46188601498c03dc04
EnocDE <enocescazan@hotmail.com>
2024-04-07 14:30:16 -0600

    Ajustes en los archivos y las extensiones

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// Archivo principal
-var server_1 = require("./server");
-console.log('Desde index.ts');
-(0, server_1.sumar)();
-//# sourceMappingURL=index.js.map
